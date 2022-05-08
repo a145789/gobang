82556749ec2e61f184939478f6e8f2c1225ebf36
a145789 <2091927351@qq.com>
2022-05-08 23:23:06 +0800

    feat: 重构

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,70 @@
 import { useEffect, useMemo, useState } from 'react'
+import swal from 'sweetalert'
+import { RadioGroup, Radio } from '@palmerhq/radio-group'
+import '@palmerhq/radio-group/styles.css' // use the default styles
+
 import './App.css'
 
-interface CheckerboardData {
-  position: [number, number]
-  style: {
-    top: number
-    left: number
-    opacity: Status
-    backgroundColor: Player
-  }
-}
+const board = Array.from({ length: 12 })
+const piecesBoard = Array.from({ length: 13 })
 
-const checkerboard = Array.from({ length: 20 })
-
-const genCheckerboardData = (): CheckerboardData[][] => {
-  const temp = Array.from({ length: 21 })
-
-  return temp.map((i1, yIdx) =>
-    temp.map((i2, xIdx) => ({
-      position: [xIdx, yIdx],
-      style: {
-        top: yIdx * 27 - 6,
-        left: xIdx * 27 - 6,
-        opacity: Status.hide,
-        backgroundColor: Player.white
-      }
+const initPieces = () =>
+  piecesBoard.map((_1, yi) =>
+    piecesBoard.map((_2, xi) => ({
+      top: yi * 28 - 7,
+      left: xi * 28 - 7,
+      bg: Piece.Empty
     }))
   )
-}
 
-const enum Player {
-  white = 'white',
-  black = 'black'
+const enum Piece {
+  Empty = '',
+  Black = 'bg-#000',
+  White = 'bg-#fff'
 }
 
 const enum Status {
-  hide,
-  show
+  Begin,
+  Select,
+  Rest
 }
 
 function App() {
-  const [begin, setBegin] = useState(true)
-  const [currentPlayer, setCurrentPlayer] = useState(Player.white)
-  const [currentPosition, setCurrentPosition] = useState<[number, number]>([
-    0, 0
-  ])
-  const [checkerboardData, setCheckerboardData] =
-    useState<CheckerboardData[][]>(genCheckerboardData)
+  const [pieces, setPieces] =
+    useState<{ top: number; left: number; bg: Piece }[][]>(initPieces)
+
+  const [currentPiece, setCurrentPiece] = useState(Piece.Empty)
+  const [currentStatus, setCurrentStatus] = useState<Status>(Status.Begin)
+
+  function fail(y: number, x: number) {
+    if (pieces[y][x].bg) {
+      return
+    }
+  }
+
+  function begin() {
+    setCurrentStatus(Status.Select)
+  }
+
+  function Select(value: Piece) {
+    setCurrentPiece(value)
+    setCurrentStatus(Status.Rest)
+  }
+
+  function rest() {
+    setCurrentPiece(Piece.Empty)
+    setCurrentStatus(Status.Begin)
+    setPieces(initPieces())
+  }
 
   const table = useMemo(
     () => (
-      <table>
+      <table className="border-collapse border border-#442C21 w-336px h-336px">
         <tbody>
-          {checkerboard.map((i1, trIdx) => (
+          {board.map((_1, trIdx) => (
             <tr key={trIdx}>
-              {checkerboard.map((i2, tdIdx) => (
-                <td key={tdIdx}></td>
+              {board.map((_2, tdIdx) => (
+                <td className="border border-#FFFDFE" key={tdIdx} />
               ))}
             </tr>
           ))}
@@ -65,119 +74,56 @@ function App() {
     []
   )
 
-  const keep = (style: CheckerboardData['style']) =>
-    style.opacity === Status.show && style.backgroundColor === currentPlayer
-
-  const judge = () => {
-    const [x, y] = currentPosition
-    // 横
-    let start = x - 4 < 0 ? 0 : x - 4
-    let end = x + 4 > 20 ? 20 : x + 4
-    let count = 0
-    for (let index = start; index <= end; index++) {
-      if (keep(checkerboardData[y][index].style)) {
-        if (++count === 5) {
-          return true
-        }
-      } else {
-        count = 0
-      }
-    }
-    // 左斜
-    count = 0
-    let leftIncline = y - 4 < 0 ? 0 : y - 4
-    for (let index = start; index <= end; index++) {
-      if (
-        !checkerboardData[leftIncline] ||
-        !checkerboardData[leftIncline][index]
-      )
-        continue
-      if (keep(checkerboardData[leftIncline++][index].style)) {
-        if (++count === 5) {
-          return true
-        }
-      } else {
-        count = 0
-      }
-    }
-
-    // 竖
-    start = y - 4 < 0 ? 0 : y - 4
-    end = y + 4 > 20 ? 20 : y + 4
-    count = 0
-    for (let index = start; index <= end; index++) {
-      if (keep(checkerboardData[index][x].style)) {
-        if (++count === 5) {
-          return true
-        }
-      } else {
-        count = 0
-      }
-    }
-
-    // 右斜
-    count = 0
-    let rightIncline = x + 4 > 20 ? 20 : x + 4
-    for (let index = start; index <= end; index++) {
-      if (!checkerboardData[index] || !checkerboardData[index][index]) continue
-      if (keep(checkerboardData[index][rightIncline--].style)) {
-        if (++count === 5) {
-          return true
-        }
-      } else {
-        count = 0
-      }
-    }
-    return false
-  }
-
-  const fall = (position: [number, number]) => {
-    setBegin(false)
-    const [x, y] = position
-    if (checkerboardData[y][x].style.opacity === Status.show) {
-      return
-    }
-    checkerboardData[y][x].style = {
-      ...checkerboardData[y][x].style,
-      opacity: Status.show,
-      backgroundColor: currentPlayer
-    }
-
-    setCheckerboardData([...checkerboardData])
-    setCurrentPosition(position)
-  }
-
-  useEffect(() => {
-    if (!begin) {
-      if (judge()) {
-        setBegin(true)
-        setCurrentPlayer(Player.white)
-        setCheckerboardData(genCheckerboardData())
-        setTimeout(() => {
-          alert(`${currentPlayer}赢了`)
-        })
-      } else {
-        setCurrentPlayer(
-          currentPlayer === Player.white ? Player.black : Player.white
+  const handle = useMemo(() => {
+    switch (currentStatus) {
+      case Status.Begin:
+        return (
+          <span className="text-#fff underline" onClick={() => begin()}>
+            开始
+          </span>
         )
-      }
+
+      case Status.Select:
+        return (
+          <RadioGroup
+            labelledBy="选择先手"
+            value={currentPiece}
+            onChange={Select}
+          >
+            <Radio value={Piece.Black}>黑子</Radio>
+            <Radio value={Piece.White}>白子</Radio>
+          </RadioGroup>
+        )
+
+      case Status.Rest:
+        return (
+          <span className="text-#fff underline" onClick={() => rest()}>
+            重置
+          </span>
+        )
+
+      default:
+        return
     }
-  }, [checkerboardData])
+  }, [currentStatus])
 
   return (
-    <div className="container">
-      {checkerboard.map((i, index) =>
-        checkerboardData[index].map(({ position, style }) => (
-          <div
-            className="piece"
-            key={position[0] + position[1]}
-            style={style}
-            onClick={() => fall(position)}
-          />
-        ))
-      )}
+    <div className="w-full flex pt-130px flex-col items-center">
+      <div className="relative">
+        {pieces.map((item, yi) =>
+          item.map(({ top, left, bg }, xi) => (
+            <div
+              className={`w-14px h-14px rounded-full absolute ${bg}`}
+              key={xi}
+              style={{ top, left }}
+              onClick={() => fail(yi, xi)}
+            />
+          ))
+        )}
+        {table}
 
-      {table}
+        <div className="flex justify-center mt-4">{handle}</div>
+      </div>
     </div>
   )
 }
